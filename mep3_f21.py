e99aa4e4e72250c4687f234bb3363449e3aa98e8
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2021-12-15 12:16:27 -0500

    Grade distribution.

--- mep3_f21.py
+++ mep3_f21.py
@@ -99,11 +99,10 @@ if selection == "Feedback from students":
 if selection == "Grade distribution":
   st.markdown('## MEP3 Historical Gradebook Dashboard')
   st.write("This dashboard summarizes student grades in semesters when Dr. Narendranath was one of the instructors of MEP3.")
-  st.write(" ")
-  df_grades = dd.read_csv('*_*.csv')
-  
+  st.write(" ")  
   sem = st.selectbox('Select semester of interest:', ['Fall 2017', 'Spring 2018', 'Fall 2018', 'Spring 2019', 'Fall 2019', 'Fall 2020', 'Spring 2021', 'Fall 2021'])
   ts =  st.slider('Select threshold final score:', max_value = 100, min_value=0)
 
+  df_grades = dd.read_csv('*_*.csv')
   st.write(sem)
-  df_grades[df_grades["Semester"] == sem]
+  #df_grades[df_grades["Semester"] == sem]
