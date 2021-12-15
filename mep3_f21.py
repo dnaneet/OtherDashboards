d790070f5c09dc0f71206e2b6603764c5ccf1a95
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2021-12-15 11:54:23 -0500

    Grade distribution.

--- mep3_f21.py
+++ mep3_f21.py
@@ -105,4 +105,29 @@ if selection == "Grade distribution":
   sem = st.selectbox('Select semester of interest:', ['Fall 2017', 'Spring 2018', 'Fall 2018', 'Spring 2019', 'Fall 2019', 'Fall 2020', 'Spring 2021', 'Fall 2021'])
   ts =  st.slider('Select threshold final score:', max_value = 100, min_value=0)
   
-  col1, col2, col3  = st.beta_columns(3)
+  col1, col2, col3  = st.columns(3)
+  with col1:
+    st.markdown("#### Total students in the selected semester:")
+    #total_students_this_sem = df[(df['Semester'] == sem)['Final Score'].count().compute()
+    df_temp = pd.DataFrame({'Final Score': df[df['Semester'] == sem]['Final Score'].compute(), 'Threshold met':df[df['Semester'] == sem]['Final Score'].compute()>=ts })
+    #st.table(df_temp)	
+    total =  df[df['Semester'] == sem]['Final Score'].count().compute()
+    st.write(total)
+  with col2:
+    st.markdown('#### Number of students with final score >= threshold score:')
+    met_threshold = df[(df['Semester'] == sem) & (df['Final Score'] >= ts)]['Final Score'].count().compute()
+    st.write(df[(df['Semester'] == sem) & (df['Final Score'] >= ts)]['Final Score'].count().compute())
+  with col3:
+    st.markdown("#### Percentage of students with final score >= threshold score:")
+    st.write(np.round(met_threshold*100/total,1))
+
+  st.markdown("#### Class average and standard deviation in the selected semester:")
+  st.write("(", np.round(df[df['Semester'] == sem]['Final Score'].mean().compute(),1), ",", np.round(df[df['Semester'] == sem]['Final Score'].std().compute(),1),")")
+
+
+
+st.markdown("#### Grade Distribution in selected semester")
+fig = px.histogram(df_temp['Final Score'])
+fig.update_xaxes(range=[0, 100])
+fig.update_yaxes(range = [0, df['Final Score'].max().compute() + 5])
+st.plotly_chart(fig)
