d4768520e4c5796fb474156cbec6c48d6e021911
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2021-12-15 11:19:07 -0500

    feedback -- table

--- mep3_f21.py
+++ mep3_f21.py
@@ -70,8 +70,8 @@ if selection == "Feedback from students":
      - 5-6 member design teams (instead of 3-4 member teams).
      - Two design reviews with DR-1 providing intermediate feedback on your design(instead of a single final presentation).
     '''
-    
-    st.table(df)
+
+    st.table(df["comments"])
 
 if selection == "Grade distribution":
     st.markdown("TBA")    
